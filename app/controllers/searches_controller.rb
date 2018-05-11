3c068b0ab5f121eb5652361e552c220f5b3dd1e9
Egor Tkachenko <=>
2018-05-11 11:44:42 +0300

    Clear some code

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,8 @@
 class SearchesController < ApplicationController
   def show
-    # binding.pry
     if params[:search]
-      # @arrival_station = RailwayStation.find(params[:search][:arrival_station])
-      # @departure_station = RailwayStation.find(params[:search][:departure_station])
       @routes = Route.all_routes_by_query(params[:search][:arrival_station],
                                           params[:search][:departure_station])
-      # binding.pry
     end
   end
 end
