e3a5b2c8a09fed5f1f375d1d753c1d8e94862fcb
Egor Tkachenko <=>
2018-05-14 11:04:13 +0300

    Finish lesson-17. Customize devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admin_trains_path
+    else
+      root_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
+
 
   namespace :admin do
     resources :railway_stations do
