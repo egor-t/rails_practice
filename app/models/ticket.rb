7bdc17623be4bff2fb808a66a67f040134ad1bf2
Egor Tkachenko <=>
2018-05-15 16:52:44 +0300

    Mailer for prod

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,4 +3,16 @@ class Ticket < ApplicationRecord
   belongs_to :train
   belongs_to :base_station, class_name: 'RailwayStation', foreign_key: :base_station_id
   belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id
+
+  after_create :send_notification
+
+  def route_name
+    "#{base_station.title} - #{end_station.title}"
+  end
+
+  private
+
+  def send_notification
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514072629) do
+ActiveRecord::Schema.define(version: 20180515123236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180514072629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "seating_place"
+    t.index ["id", "type"], name: "index_carriages_on_id_and_type"
     t.index ["train_id"], name: "index_carriages_on_train_id"
   end
 
